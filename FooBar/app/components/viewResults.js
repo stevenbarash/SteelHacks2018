f59ac3382063f4aa284661d7242e6e298333dbe1
Roman Kasparian <roman.kasparian@gmail.com>
2018-02-25 14:42:00 -0500

    fixed

--- FooBar/app/components/viewResults.js
+++ FooBar/app/components/viewResults.js
@@ -7,57 +7,49 @@ export default class ResultsScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-results:[]
+      results: []
     };
   }
   componentDidMount() {
     this.loadResults();
   }
   loadResults() {
-  	  fetch('http://52.170.251.39:3000/getRestaurants')
-    .then((response) => response.json())
-    .then((responseJson) => {
-    	console.log(responseJson);
-    	this.setState({results: responseJson.sort((a, b) => {
-        if (a.score > b.score)
-            return -1;
-        if (a.score < b.score)
-            return 1;
-        return 0;
-    })})
-      return responseJson;
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-    .catch((err) => {
-    console.log(err)
-    });
-    // getResults(params)
-    //   .then(data => {
-    //     console.log("requested Results successfully");
-
-    //     for (var i = 0; i < data.value.length; i++) {
-    //       data.value[i].key = i;
-    //     }
-    //     this.setState({ data: data.value, length: data.value.length });
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   });
+    fetch('http://52.170.251.39:3000/getRestaurants')
+      .then((response) => {
+        console.log(response);
+        response.json().then((responseJson) => {
+          console.log(responseJson);
+          this.setState({
+            results: responseJson.sort((a, b) => {
+              if (a.score > b.score)
+                return -1;
+              if (a.score < b.score)
+                return 1;
+              return 0;
+            })
+          })
+          return responseJson;
+        })
+          .catch((error) => {
+            console.error(error);
+          })
+          .catch((err) => {
+            console.log(err)
+          });
+      });
   }
 
   render() {
     return (
-      <View style={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={styles.welcome}>
           FooBar Results
         </Text>
         <FlatList
-        style={{ flex: 1 }}
-        data={this.state.results}
-        renderItem={({ item }) => <RestuarantScreen key={item.id} score={item.score} id={item.id}/>}
-      />
+          style={{ flex: 1 }}
+          data={this.state.results}
+          renderItem={({ item }) => <RestuarantScreen key={item.id} score={item.score} id={item.id} />}
+        />
       </View>
     );
   }
