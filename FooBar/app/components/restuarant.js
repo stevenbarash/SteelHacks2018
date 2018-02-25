3d364efbf85cef30f14e42a8474d8b45b5b5db03
Surbhi Patel <surbster98@gmail.com>
2018-02-25 13:50:17 -0500

    more results

---
+++ FooBar/app/components/restuarant.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform, TouchableOpacity, Button, TouchableHighlight, FlatList } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import {getRestaurant} from './../util/yelp';
+
+export default class RestuarantScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    resuarant:{}
+    };
+  }
+  componentDidMount() {
+    this.loadRestuarants();
+  }
+  loadRestuarants() {
+  	  getRestaurant(this.props.id).then((response) => {
+  	  	this.setState({resuarant:response})
+  	  })
+    .catch((error) => {
+      console.error(error);
+    });
+    // getRestuarants(params)
+    //   .then(data => {
+    //     console.log("requested Restuarants successfully");
+
+    //     for (var i = 0; i < data.value.length; i++) {
+    //       data.value[i].key = i;
+    //     }
+    //     this.setState({ data: data.value, length: data.value.length });
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
+  }
+
+  render() {
+    return (
+      <View style={{padding:20, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>{this.state.resuarant.name}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ FooBar/app/components/viewResults.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform, TouchableOpacity, Button, TouchableHighlight, FlatList } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import RestuarantScreen from './restuarant';
+
+export default class ResultsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+results:[]
+    };
+  }
+  componentDidMount() {
+    this.loadResults();
+  }
+  loadResults() {
+  	  fetch('http://52.170.251.39:3000/getRestaurants')
+    .then((response) => response.json())
+    .then((responseJson) => {
+    	console.log(responseJson);
+    	this.setState({results: responseJson})
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+    // getResults(params)
+    //   .then(data => {
+    //     console.log("requested Results successfully");
+
+    //     for (var i = 0; i < data.value.length; i++) {
+    //       data.value[i].key = i;
+    //     }
+    //     this.setState({ data: data.value, length: data.value.length });
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
+  }
+
+  render() {
+    return (
+      <View style={{flexGrow: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={styles.welcome}>
+          FooBar Results
+        </Text>
+        <FlatList
+        style={{ flex: 1 }}
+        data={this.state.results}
+        renderItem={({ item }) => <RestuarantScreen key={item.id} id={item.id}/>}
+      />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
